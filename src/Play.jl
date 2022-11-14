47ec68826e61ae61fcbf96ccaebc07b01286313f
KwatMDPhD <kwat.me@icloud.com>
2022-11-14 15:08:30 -0700

    1st pro

--- src/Play.jl
+++ src/Play.jl
@@ -8,6 +8,14 @@ using BenchmarkTools
 
 using DataFrames
 
-using Random
+#using Random
 
 using OnePiece
+
+function tabulate(st)
+
+    OnePiece.DataFrame.make([
+        split(li, "\t") for li in split(st, "\n", keepempty = false) if !startswith(li, "#")
+    ])
+
+end
